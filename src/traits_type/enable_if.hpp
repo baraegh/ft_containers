bd002e9375f0faaa42bc3d04ec0abcfe6223b6a9
baraegh <barae.gh@gmail.com>
2023-01-21 18:49:14 +0100

    implement ft::enable_if & std::is_integral traits types

---
+++ src/traits_type/enable_if.hpp
@@ -0,0 +1,17 @@
+
+
+#ifndef ENABLE_IF_HPP
+# define ENABLE_IF_HPP
+
+namespace ft {
+    template<bool b, class T = void>
+    struct  enable_if {};
+
+    template<class T>
+    struct enable_if<true, T>
+    {
+        typedef T type;
+    };
+}
+
+#endif
